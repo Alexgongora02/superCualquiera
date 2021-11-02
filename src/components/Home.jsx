d89da8fbd07f07fdc47206e7c2186cf1a1c6a9fa
Mauricio Romero <mauricionromero@hotmail.com>
2021-11-02 14:11:15 -0300

    añadido barra categorias

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProductList from "./ProductList";
 import Carousel from "./Carousel";
+import Categorias from "./Categorias";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Carousel />
+      <Categorias />
       <ProductList />
     </div>
   );
