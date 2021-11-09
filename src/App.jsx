a48846d8e611ca1659c92c7ba1776c2cef0f330e
ChinoBraca <informaticayseguridad1808@gmail.com>
2021-11-08 21:29:53 -0300

    Creacion de Formulario de Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Form from "./components/Form";
 import Home from "./components/Home";
+
 import "./App.css";
 
 function App() {
@@ -13,9 +15,10 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Form} />
             <Route path="/login" render={() => <h1>Login</h1>} />
-            <Route path="/contact" render={() => <h1>Contact</h1>} />
           </Switch>
+            <Route path="/contact" render={() => <Form/>} />
         </div>
         <footer className="mx-auto bg-primary text-center text-white mt-3 pb-1">
           <h4>Footer</h4>
