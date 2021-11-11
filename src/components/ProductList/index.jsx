2c9e4912e4cd84d87ff15314a25097978b5ea85e
Mauricio Romero <mauricionromero@hotmail.com>
2021-11-11 17:02:14 -0300

    los productos de la pantalla principal se muestran directamete de firestore

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchStore } from "./../../store/actions/actions";
 import ProductCard from "./ProductCard";
 
-
 export default function Index() {
+  const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
+
+  //use useEffect to call one time when component is mounted
+  useEffect(() => {
+    dispatch(fetchStore());
+  }, [dispatch]);
+
   return (
     <div className="mx-auto bg-light p-4 rounded row">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {products.length > 0 ? (
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      ) : (
+        <div className="text-center">
+          <div className="spinner-grow text-primary" role="status">
+            <span className="sr-only"></span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
