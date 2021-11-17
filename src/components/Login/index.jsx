00ffd5f197f1895bcf37afd2faa26767327af205
Mauricio Romero <mauricionromero@hotmail.com>
2021-11-16 22:23:30 -0300

    cambios rutas

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Login() {
+  return (
+    <div>
+    
+      <button type="button" className="btn btn-outline-secondary">
+        Mi Cuenta✔
+      </button>
+    </div>
+  );
+}
