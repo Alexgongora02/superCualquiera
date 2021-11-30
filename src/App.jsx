ab9f11ca59d93c9fbd249c77a40f13ee946b567b
marie <marielabracamonte83@gmail.com>
2021-11-29 23:32:30 -0300

    se creo el dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,53 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Form from "./components/Form";
 import Home from "./components/Home";
+import {useAuth0} from "@auth0/auth0-react"
+
 import UploadProducts from "./components/UploadProducts";
-import { Auth0Provider } from "@auth0/auth0-react";
 import Login from "./components/Login";
+
+import Logout from "./components/Logout";
+import {Profile} from "./components/Profile";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Categorias from "./components/Navbar/Categorias";
 import Lista from "./components/Navbar/Lista";
 import PagoDeCompra from "./components/PagoDeCompra";
 import Pedidos from "./components/Pedidos";
+import Dashboard from "./components/Dashboard/Dashboard";
 import "./App.css";
 
 function App() {
+  const {isAuthenticated} = useAuth0()
   return (
     <Router>
-      <Auth0Provider
-    domain="dev-2z251kkp.us.auth0.com"
-    clientId="iRTuS1osHTxttMzCXCngs2otqlkY0CjD"
-    redirectUri={window.location.origin}
-  >
+      
+  
       <Provider store={store}>
         <Navbar />
         <div className="container container-main">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/form" component={Form} />
-            <Route path="/login" component={Login} />
+            <Route path="/login" component={Login}/>   
+            <Route path="/profile" component ={Profile}/>
+            <Route path="/logout" component={Logout}/>
             <Route path="/upload" component={UploadProducts} />
             <Route path="/categorias" component={Categorias} />
             <Route path="/lista" component={Lista} />
             <Route path="/pago" component={PagoDeCompra} />
             <Route path="/pedidos" component={Pedidos} />
+            <Route path="/dashboard" component={Dashboard} />
+            
+            
+        
           </Switch>
         </div>
         <Footer>
           <h4>Footer</h4>
         </Footer>
       </Provider>
-      </Auth0Provider>,
+     
     </Router>
   );
 }
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>MI CUENTA (¬‿¬) </button>;
+  return (<button onClick={() => loginWithRedirect()}>MI CUENTA</button>
+  
+  );
 };
 
-export default LoginButton;
+export default Login;
 
 
