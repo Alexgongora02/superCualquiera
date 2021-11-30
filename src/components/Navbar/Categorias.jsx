11976e0a41473788f73a8436a7b03c99537a24e8
Alexis Gongora <alexisgongora02@gmail.com>
2021-11-30 09:33:22 -0300

    Categorias

--- src/components/Navbar/Categorias.jsx
+++ src/components/Navbar/Categorias.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
+export default function Categorias({ setFilter, filter }) {
+  const categorias = useSelector((state) => state.categorias);
 
-export default function Categorias() {
   return (
-    <div>
-      <h1>Hola soy Categorias</h1>
+    <div className="btn-group mx-auto my-4 d-flex flex-wrap" role="group">
+      <button
+        type="button"
+        className={`btn btn-outline-primary rounded-0 ${!filter && `active`}`}
+        onClick={() => setFilter(false)}
+        >
+         Todos
+        </button>
+      {categorias.map((categoria) => (
+        <button
+          type="button"
+          className={`btn btn-outline-primary rounded-0 text-capitalize ${filter === categoria && `active`}`}
+          key={categoria}
+          onClick={() => setFilter(categoria)}
+        >
+          {categoria}
+        </button>
+      ))}
     </div>
-  )
+  );
 }
